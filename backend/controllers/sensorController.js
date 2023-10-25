fb09721b04990b2651c399d8935c3f402b0da39c
rlangtinn95 <richalan@stud.ntnu.no>
2023-10-25 13:12:20 +0200

    added apikey and other improvements to router

--- backend/controllers/sensorController.js
+++ backend/controllers/sensorController.js
@@ -1,18 +1,40 @@
 const express = require('express');
+const axios = require('axios');
 const DataModel = require('../models/wotSchema');
 
 const router = express.Router();
 
-// POST route to save data
+//POST route to post data
 router.post('/model', async (req, res) => {
   try {
-    const newData = new DataModel(req.body);
+    const { latitude, longitude } = req.body.location;
+    const apiKey = 'e4818876205ff3c5226eff1dedfe15e5'; 
+    const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+    const weatherApiResponse = await axios.get(weatherApiUrl);
+    const weatherData = weatherApiResponse.data;
+
+    const newData = new DataModel({
+      location: req.body.location,
+      interiorTemperature: req.body.interiorTemperature,
+      weather: {
+        temperature: weatherData.main.temp,
+        conditions: weatherData.weather[0].main,
+        uvIndex: 0, // openweathermap does not seem to provide a uvIndex, another weatherAPI maybe??
+        sunrise: new Date(weatherData.sys.sunrise * 1000),
+        sunset: new Date(weatherData.sys.sunset * 1000),
+        // need strongestLightTime and weakestLightTime as well, but how is that calculated?
+      },
+    });
+
+  
     const savedData = await newData.save();
     res.status(201).json(savedData);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
 });
+
 
 // GET route to retrieve data
 router.get('/model', async (req, res) => {
@@ -23,5 +45,7 @@ router.get('/model', async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
+// place for update and delete data as well, need to check that out tomorrow
 
 module.exports = router;
