ba84ef2418982e2748fb246812aacd28ecd20d2f
rlangtinn95 <richalan@stud.ntnu.no>
2023-11-18 05:28:43 +0100

    small changes

--- my-react-app/src/components/Weather.jsx
+++ my-react-app/src/components/Weather.jsx
@@ -1,4 +1,3 @@
-// src/components/Weather.js
 import React, { useState } from 'react';
 import axios from 'axios';
 import './Weather.css';
@@ -11,23 +10,27 @@ const Weather = () => {
   const API_URL = 'https://api.open-meteo.com/v1/forecast';
 
   const getWeather = async () => {
-      try {
-        const response = await axios.get(API_URL, {
-          params: {
-            latitude: latitude,
-            longitude: longitude,
-            current: 'temperature_2m,is_day,wind_speed_10m',
-            hourly: 'temperature_2m,precipitation,rain,showers,snowfall,snow_depth,wind_speed_10m,uv_index,is_day,sunshine_duration',
-            forecast_days: 14,
-          },
-        });
-        console.log(response.data); // Log the entire response to the console
-        setWeatherData(response.data);
-      } catch (error) {
-        console.error('Error fetching weather data:', error);
-      }
-    };
-    
+    try {
+      const response = await axios.get(API_URL, {
+        params: {
+          latitude: latitude,
+          longitude: longitude,
+          current: 'temperature_2m,is_day,wind_speed_10m,uv_index',
+          hourly: 'temperature_2m,precipitation,rain,showers,snowfall,snow_depth,wind_speed_10m,uv_index,is_day,sunshine_duration',
+          daily: 'temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum,rain_sum',
+          daily_days: 14,
+        },
+      });
+      console.log(response.data); // Log the entire response to the console
+
+      const { hourly, daily, ...rest } = response.data;
+
+      // Store hourly and daily data separately in the state
+      setWeatherData({ hourly, daily, ...rest });
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+  };
 
   return (
     <div className="weather-container">
@@ -57,39 +60,32 @@ const Weather = () => {
             <p>Temperature: {weatherData.current.temperature_2m} °C</p>
             <p>Is Day: {weatherData.current.is_day ? 'Yes' : 'No'}</p>
             <p>Wind Speed: {weatherData.current.wind_speed_10m} m/s</p>
+            <p>UV Index: {weatherData.current.uv_index}</p>
           </div>
 
           <div className="hourly-weather">
-  <h3>Hourly Forecast</h3>
-  {Array.isArray(weatherData.hourly) ? (
-    <ul>
-      {weatherData.hourly.map((hour, index) => (
-        <li key={index}>
-          Time: {hour.time}, Temperature: {hour.temperature_2m} °C
-          {/* Add more hourly details as needed */}
-        </li>
-      ))}
-    </ul>
-  ) : (
-    <p>No hourly data available</p>
-  )}
-</div>
+            <h3>Hourly Forecast</h3>
+            {weatherData.hourly && (
+              <ul>
+                <li>
+                  Time: {weatherData.hourly.time}, Temperature: {weatherData.hourly.temperature_2m} °C
+                  {/* Add more hourly details as needed */}
+                </li>
+              </ul>
+            )}
+          </div>
 
-<div className="daily-forecast">
-  <h3>Daily Forecast (Next 14 Days)</h3>
-  {Array.isArray(weatherData.hourly) ? (
-    <ul>
-      {weatherData.hourly.map((day, index) => (
-        <li key={index}>
-          Date: {day.time}, Max Temperature: {day.temperature_2m_max} °C, Min Temperature: {day.temperature_2m_min} °C
-          {/* Add more daily forecast details as needed */}
-        </li>
-      ))}
-    </ul>
-  ) : (
-    <p>No daily forecast data available</p>
-  )}
-</div>
+          <div className="daily-forecast">
+            <h3>Daily Forecast (Next 14 Days)</h3>
+            {weatherData.daily && (
+              <ul>
+                <li>
+                  Date: {weatherData.daily.time}, Max Temperature: {weatherData.daily.temperature_2m_max} °C, Min Temperature: {weatherData.daily.temperature_2m_min} °C
+                  {/* Add more daily forecast details as needed */}
+                </li>
+              </ul>
+            )}
+          </div>
         </div>
       )}
     </div>
@@ -97,6 +93,8 @@ const Weather = () => {
 };
 
 export default Weather;
+
+
 
 
 
